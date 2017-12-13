5c6fef93ad524a0a313522310ddd31d290d91a5d
root <root@vps492167.ovh.net>
2017-12-13 09:31:50 +0100

    OK : 2nd try

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 require 'classes/HtmlDocument.class.php';
 require 'classes/PageInexistanteException.php';
 //require 'libs/mobile.lib.php';
-//TRY THIS
 $page = isset($_GET['page']) ? $_GET['page'] : 'index';
 $doc = new HtmlDocument($page);
 $doc->applyTemplate('default');
